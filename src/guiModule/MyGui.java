7e845354704b73b7b804427523b45c6e716950df
Bealfan <bealfan@Bealfan-MacBook-Pro-2.local>
2019-04-14 12:17:36 -0500

    init commit

---
+++ src/guiModule/MyGui.java
@@ -0,0 +1,26 @@
+package guiModule;
+
+import processing.core.PApplet;
+
+public class MyGui extends PApplet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void setup() {
+		
+		size(400,400);
+		background(255,255,255); 
+		
+		
+	}
+	
+	public void draw() {
+		
+		ellipse(56, 46, 55, 55); 
+		
+	}
+
+}
